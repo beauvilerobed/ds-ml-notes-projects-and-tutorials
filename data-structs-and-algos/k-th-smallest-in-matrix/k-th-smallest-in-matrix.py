cb8626b488ac4cc01fd734d0cf382ed2e9582417
beauvilerobed <robedbeauvil@gmail.com>
2023-05-31 16:58:25 -0400

    add problem statement

---
+++ data-structs-and-algos/k-th-smallest-in-matrix/k-th-smallest-in-matrix.py
@@ -0,0 +1,7 @@
+# Say you have an n-by-n matrix of elements that are sorted in asc order both in
+# the columns and rows of the matrix. Return the k-th smallest element of the matrix.
+
+
+# Example: A = [[1,4,7],[3,5,9],[6,8,11]]
+# If k=4 return 5
+
