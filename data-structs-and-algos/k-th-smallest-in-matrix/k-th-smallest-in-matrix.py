4dba3d89c346c420f89a12dbc8deadb60251d22d
beauvilerobed <robedbeauvil@gmail.com>
2023-06-02 17:19:59 -0400

    update k th smallest in matrix with first solution

--- data-structs-and-algos/k-th-smallest-in-matrix/k-th-smallest-in-matrix.py
+++ data-structs-and-algos/k-th-smallest-in-matrix/k-th-smallest-in-matrix.py
@@ -2,6 +2,45 @@
 # the columns and rows of the matrix. Return the k-th smallest element of the matrix.
 
 
-# Example: A = [[1,4,7],[3,5,9],[6,8,11]]
+# Example: A = [[1,4,7],
+#.              [3,5,9],
+#.              [6,8,11]]
 # If k=4 return 5
+
+from heapq import heappush, heappop
+
+def k_smallest(matrix, k):
+    n = len(matrix)
+    m = len(matrix[0])
+    
+    min_heap = []
+
+    for i in range(n):
+        for j in range(m):
+            heappush(min_heap, matrix[i][j])
+
+    res = 0
+    for _ in range(k):
+        res = heappop(min_heap)
+    
+    return res
+
+def main():
+    k = int(input())
+    n = int(input())
+    m = int(input())
+    matrix = []
+    for _ in range(n):
+        arr = input().split()
+        arr = [float(num) for num in arr]
+        if len(arr) == m:
+            matrix.append(arr)
+
+    print(k_smallest(matrix, k))
+
+if __name__ == '__main__':
+    main()
+
+    
+
 
