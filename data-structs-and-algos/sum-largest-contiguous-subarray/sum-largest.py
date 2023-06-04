982e06d83d05daae82b435dc1171f315204e8c17
beauvilerobed <robedbeauvil@gmail.com>
2023-06-04 10:29:37 -0400

    updated problem statement

---
+++ data-structs-and-algos/sum-largest-contiguous-subarray/sum-largest.py
@@ -0,0 +1,6 @@
+# Given an integer array, find the sum of the largest contiguous subarray within the array.
+
+# Example A = [-1, -3, 5, -4, 3, -6, 9, 2] returns 11 since [9,2].
+
+# note: if all the elements are negative, you should return 0
+
