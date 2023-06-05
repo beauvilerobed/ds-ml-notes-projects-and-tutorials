5dcac880aca6aee620a1aefe12762dbd236340b1
beauvilerobed <robedbeauvil@gmail.com>
2023-06-05 16:13:52 -0400

    chore: rename file name

---
+++ data-structs-and-algos/binary-tree-mirror-image/mirror-image.py
@@ -0,0 +1,7 @@
+# Given a binary tree, write a function to determine whether the tree is a mirror
+# image of itself. Two trees are a mirror image of each other if their root values
+# are the same and the left subtree is a mirror image of the right subtree.
+from binarytreegenerator import Node
+
+def check_sym(node):
+    
