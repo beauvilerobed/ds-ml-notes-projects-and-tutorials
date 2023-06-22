510ac62c9a28c824bd4581044a94445bba9adfec
beauvilerobed <robedbeauvil@gmail.com>
2023-06-22 14:11:12 -0400

    chore: update social-graph to generate graph

--- data-structs-and-algos/social-graph/social-graph.py
+++ data-structs-and-algos/social-graph/social-graph.py
@@ -2,14 +2,28 @@
 # to return the smallest number of friendship between the two users.
 
 # Example: (A, B),(A, C),(B, D),(D, E) input: A, E return: 3
-
+from graphs import Graph, Vertex
 
 def least_friends(v1, v2):
     pass
 
 
 def main():
-    pass
+    graph = Graph()
+    val = 0
+    while val != 'stop':
+        val = input()
+        if val == 'stop':
+            break
+
+        val = val.split()
+        if val[0] == 'add':
+            graph.add_vertex(val[1])
+
+        if val[0] == 'edges':
+            graph.add_edge(val[1], val[2], 0)
+    
+    print(graph.get_vertices())
 
 
 if __name__ == '__main__':
