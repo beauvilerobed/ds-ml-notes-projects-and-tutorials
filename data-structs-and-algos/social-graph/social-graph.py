36a9258f9be82283a22b338f59bb7bc2d6a09998
beauvilerobed <robedbeauvil@gmail.com>
2023-06-22 13:50:07 -0400

    task: update with problem statement

---
+++ data-structs-and-algos/social-graph/social-graph.py
@@ -0,0 +1,16 @@
+# Given a social graph and two users as input, write a function
+# to return the smallest number of friendship between the two users.
+
+# Example: (A, B),(A, C),(B, D),(D, E) input: A, E return: 3
+
+
+def least_friends(v1, v2):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
