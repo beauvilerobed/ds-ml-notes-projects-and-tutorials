057d595d1f6add30159ef0b384d139fc185b34a3
beauvilerobed <robedbeauvil@gmail.com>
2023-06-27 15:33:18 -0400

    update: add solution

--- data-structs-and-algos/count-number-triangle/count-number-triangle.py
+++ data-structs-and-algos/count-number-triangle/count-number-triangle.py
@@ -12,4 +12,50 @@
 #        sides = [3, 3, 3, 3] -> 4
 #        sides = [1, 2, 3.5] -> 0
 
+def to_check_to_sides(to_check, triangles):
+    # sum of two sides is greater
+    # than the third
 
+    if (to_check[0] < to_check[1] + to_check[2]) and\
+        (to_check[1] < to_check[0] + to_check[2]) and\
+         (to_check[2] < to_check[1] + to_check[0]):
+        triangles.append(to_check)
+    
+    return triangles
+
+
+def possible_triangles(arr):
+    n = len(arr)
+    triangles = []
+
+    if n <= 2:
+        return triangles
+
+    if n == 3:
+        triangles = to_check_to_sides(arr, triangles)
+        return triangles 
+    
+    for i in range(n):
+        to_check = None
+        if i+2 > n-1:
+            start = i
+            end = (i+2)%n
+            to_check = arr[:end+1]+arr[start:]
+        else:
+            to_check = arr[i:i+3]
+        
+        # sum of two sides is greater
+        # than the third
+        triangles = to_check_to_sides(to_check, triangles)
+        
+    return triangles
+
+def main():
+    arr = [float(val) for val in input().split()]
+    triangles = possible_triangles(arr)
+    print("triangles ",triangles)
+    print(len(triangles))
+
+
+if __name__ == '__main__':
+    main()
