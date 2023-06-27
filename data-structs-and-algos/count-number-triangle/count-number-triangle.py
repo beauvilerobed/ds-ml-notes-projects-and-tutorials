96477fe3b450288dc0dca06cd5e5cba6ace2a73e
beauvilerobed <robedbeauvil@gmail.com>
2023-06-27 14:09:47 -0400

    task: add solution to number triangles

---
+++ data-structs-and-algos/count-number-triangle/count-number-triangle.py
@@ -0,0 +1,15 @@
+# Given a list of at least three numbers named sides. 
+# Each entry in sides represents the length of a side of a triangle. 
+# Write a function possible_triangles to determine the number of possible 
+# triangles that can be formed using the given side lengths. 
+# In the case that more than three side lengths are given, there may be several 
+# possible triangles.
+
+# Note: If there are multiple equal side lengths in the list, we consider them as distinct sides.
+
+# Examples:
+# Input: sides = [3, 3, 3] -> 1
+#        sides = [3, 3, 3, 3] -> 4
+#        sides = [1, 2, 3.5] -> 0
+
+
